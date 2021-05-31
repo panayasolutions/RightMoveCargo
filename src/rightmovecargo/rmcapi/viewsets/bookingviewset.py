4b0ef6beda3c9ead230032af36dd3d20b603584c
Dextor <dextor.developer@gmail.com>
2021-05-31 12:39:32 +0800

    Booking Submit

--- src/rightmovecargo/rmcapi/viewsets/bookingviewset.py
+++ src/rightmovecargo/rmcapi/viewsets/bookingviewset.py
@@ -42,12 +42,39 @@ class BookingViewSet(BaseViewSet):
                 # booking.shipment = ShipmentMode.objects.filter(shipment_mode_code='CA')
                 booking.dim = Tbbookingchild.objects.filter(masterawbno=booking.awbNo)
             except Consignee.DoesNotExist :
-                booking.consignee = ''
+                booking.consignee = None
             except ShipmentMode.DoesNotExist :
-                booking.shipment = ''
+                booking.shipment = None
             except Tbbookingchild.DoesNotExist :
-                booking.dim = ''
-
+                booking.dim = None
         serializer = self.get_serializer(queryset , many=True) # self.get_serializer(request.user)
+        return self.onSuccess(serializer.data," ",status.HTTP_200_OK);
+
+    def retrieve(self, request, *args, **kwargs):
+        awbNo = kwargs.get('pk');
+        queryset = None
+        user = self.get_user(request);
+        user_type = self.get_user_type(request).type_code;
+        company_code = self.get_company(request).company_code;
+        if user_type !='ZEMP':
+            queryset = self.get_queryset().filter(user=user.userid,awbNo=awbNo)
+        else:
+            queryset = self.get_queryset().filter(companyCode=company_code,awbNo=awbNo)
+
+        for booking in queryset:
+            booking.client = Client.objects.get(userid=booking.user)
+            try:
+                booking.consignee = Consignee.objects.get(conscode=booking.consignee)
+                booking.courier = Courier.objects.get(branchcode=booking.courier)
+                booking.courier.courier_shipment = ShipmentMode.objects.filter(shipment_mode_code='CA')
+                # booking.shipment = ShipmentMode.objects.filter(shipment_mode_code='CA')
+                booking.dim = Tbbookingchild.objects.filter(masterawbno=booking.awbNo)
+            except Consignee.DoesNotExist :
+                booking.consignee = None
+            except ShipmentMode.DoesNotExist :
+                booking.shipment = None
+            except Tbbookingchild.DoesNotExist :
+                booking.dim = None
+        serializer = self.get_serializer(queryset , many=True)
         return self.onSuccess(serializer.data," ",status.HTTP_200_OK);
         
